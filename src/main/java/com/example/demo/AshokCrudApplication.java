1604564c0dee738b97e4e08dd5f5262f419f87d8
ashok7057 <108855932+ashok7057@users.noreply.github.com>
2024-03-23 02:49:29 +0000

    Update AshokCrudApplication.java

--- src/main/java/com/example/demo/AshokCrudApplication.java
+++ src/main/java/com/example/demo/AshokCrudApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AshokCrudApplication {
+public class AshokCrudApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AshokCrudApplication.class, args);
